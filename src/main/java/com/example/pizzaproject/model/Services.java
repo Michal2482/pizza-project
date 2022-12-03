a33538e9815e6fa21b17554e4c5d480744d0984c
Artur9393 <brzezinski93@gmail.com>
2022-12-03 16:09:32 +0100

    Services

---
+++ src/main/java/com/example/pizzaproject/model/Services.java
@@ -0,0 +1,59 @@
+package com.example.pizzaproject.model;
+
+import javax.persistence.*;
+
+
+@Entity (name = "Services")
+public class Services {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String name;
+    private String description;
+    private String photo;
+
+    public Services(Long id, String name, String description, String photo) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.photo = photo;
+    }
+
+    public Services() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+}
