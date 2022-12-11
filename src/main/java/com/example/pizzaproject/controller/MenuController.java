de4a2aa4770e1c6d7de5c9da7fbcf20709207165
Patryk <patryk.marcisz.v2@gmail.com>
2022-12-11 11:43:50 +0100

    dzielenie menu na kategorie; template dla diva wyswietlajacego kategorie

--- src/main/java/com/example/pizzaproject/controller/MenuController.java
+++ src/main/java/com/example/pizzaproject/controller/MenuController.java
@@ -8,7 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class MenuController {
@@ -22,12 +25,23 @@ public class MenuController {
 
     private final CategoryService categoryService;
 
-    @GetMapping ("/menu")
+    @GetMapping("/menu")
     public String getMealList(Model model) {                //wyświetla widok menu z listą dań i kategorii
         List<Meal> mealList = mealService.getMeals();
         model.addAttribute("meal", mealList);
         List<Category> categoryList = categoryService.getCategories();
+        Map<String, List<Meal>> mealByCategories = mealList.stream().collect(Collectors.toMap(
+                meal -> meal.getCategory().getCategoryName(),
+                meal -> List.of(meal),
+                (List<Meal> oldVal, List<Meal> newVal) -> {
+                    List<Meal> newList = new ArrayList();
+                    newList.addAll(oldVal);
+                    newList.addAll(newVal);
+                    return newList;
+                }
+        ));
         model.addAttribute("category", categoryList);
+        model.addAttribute("categoryMap", mealByCategories);
         return "pizzaPage/menu";
     }
 
@@ -35,7 +49,6 @@ public class MenuController {
 //    public String getAbout() {
 //        return "pizzaPage/menu";
 //    }
-
 
 
 }
