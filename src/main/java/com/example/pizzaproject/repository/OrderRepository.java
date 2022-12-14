b64c9cd79c5c748adfac330513589669a432306d
Bartek <bartekpiw1995@gmail.com>
2022-12-14 10:41:51 +0100

    Rozpoczęcie pracy nad zamowieniami

---
+++ src/main/java/com/example/pizzaproject/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.example.pizzaproject.repository;
+
+import com.example.pizzaproject.model.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order,Long> {
+}
